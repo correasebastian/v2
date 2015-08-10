8c14ab4a84e80a273fc1fc163261e7bbdc92ca1e
sebastian correa <sebastian.correa@ajustev.com>
2015-08-10 16:22:16 -0500

    deplu1

--- www/js/ajustes/ajustes.js
+++ www/js/ajustes/ajustes.js
@@ -5,15 +5,18 @@
         .module('app.ajustes')
         .controller('Ajustes', Ajustes);
 
-    Ajustes.$inject = ['ajustesService','consultaService' , 'dataInitService' , 'zumeroService', 'Sqlite' , '$state', 'authService'];
+    Ajustes.$inject = ['ajustesService','consultaService' , 'dataInitService' , 'zumeroService', 'Sqlite' , '$state', 'authService', '$ionicDeploy', 'logger'];
 
-    function Ajustes(ajustesService    ,  consultaService  , dataInitService  ,  zumeroService  , Sqlite   , $state ,  authService){
+    function Ajustes(ajustesService    ,  consultaService  , dataInitService  ,  zumeroService  , Sqlite   , $state ,  authService , $ionicDeploy , logger){
     	var vm=this;
     	vm.resetConsulta=resetConsulta;
         vm.resetDataInit=resetDataInit;
         vm.logOut=logOut;
         vm.zync=zync;
+        vm.hasUpdate=false;
 
+        vm.doUpdate=doUpdate;
+        vm.checkForUpdates=checkForUpdates;
     	//oimplementation
 
     	function resetConsulta () {
@@ -39,6 +42,31 @@
         function zync () {
             zumeroService.zync(12);
         }
+
+
+              // Update app code with new release from Ionic Deploy
+      function doUpdate() {
+        $ionicDeploy.update().then(function(res) {
+          logger.success('Ionic Deploy: Update Success! ', res);
+        }, function(err) {
+          logger.error('Ionic Deploy: Update error! ', err);
+        }, function(prog) {
+          logger.info('Ionic Deploy: Progress... ', prog);
+        });
+      };
+
+      // Check Ionic Deploy for new code
+       function checkForUpdates() {
+        clogger.success('Ionic Deploy: Checking for updates');
+        $ionicDeploy.check().then(function(hasUpdate) {
+          logger.success('Ionic Deploy: Update available: ' + hasUpdate);
+          vm.hasUpdate = hasUpdate;
+        }, function(err) {
+          logger.error('Ionic Deploy: Unable to check for updates', err);
+        });
+      }
+
+
 
 
     }
